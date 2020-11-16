346e79872e2b2f746bed84269911e31d60ecaa6b
ajilpappachan <ajilpappachan@gmail.com>
2020-11-16 13:05:44 +0530

    Added more Art

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        player1.GetComponent<PlayerController>().UpdateMovement(horizontal, vertical);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D player;
+    private float speed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateMovement(float horizontal, float vertical)
+    {
+        player.velocity = new Vector2(horizontal, vertical + Physics2D.gravity.y) * Time.deltaTime * speed;
+    }
+}
